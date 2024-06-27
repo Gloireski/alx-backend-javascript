de98c2fa4b0858b0bf07f36ddf8bd2445dba8023
Gloireski <belemgloirebekoutou8@gmail.com>
2024-06-27 15:58:43 +0000

    task 7 fix

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,6 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   const promises = [chinaDownload, USDownload];
   return Promise
-    .any(promises)
+    .race(promises)
     .then((value) => value);
 }
