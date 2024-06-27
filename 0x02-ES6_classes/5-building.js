1c67eb086f8f0275ae2d3b9d5ec4c225f90aac9f
Gloireski <belemgloirebekoutou8@gmail.com>
2024-06-27 21:09:06 +0000

    task 5 fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,12 +2,12 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (this.constructor !== Building) {
-        if (typeof this.evacuationWarningMessage !== 'function') {
-          throw new Error(
-            'Class extending Building must override evacuationWarningMessage',
-          );
-        }
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
       }
+    }
   }
 
   get sqft() {
