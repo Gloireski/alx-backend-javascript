ab9fae816890dba9640c2685c2940fa282f16d68
Gloireski <belemgloirebekoutou8@gmail.com>
2024-06-28 22:01:51 +0100

    task 10 fixed

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((element) => appendString + element);
+  const newArray = [];
+  for (const val of array) {
+    newArray.push(appendString + val);
+  }
+  return newArray;
 }
