fe9b7c8512d24a01df4ab574a395a61dad899bf0
Gloireski <belemgloirebekoutou8@gmail.com>
2024-06-28 00:32:46 +0000

    task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,14 +1,13 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this._size =  size;
+    this._size = size;
     this._location = location;
   }
 
   [Symbol.toPrimitive](hint) {
     if (hint === 'string') {
-        return this._location;
-    } else if (hint === 'number') {
-        return this._size;
+      return this._location;
     }
-}
+    return this._size;
+  }
 }
