9b2e2a9cd56ab3ac072ea8daa222d850c9cfa7ab
Gloireski <belemgloirebekoutou8@gmail.com>
2024-06-28 00:52:46 +0000

    tadsk 9 fixed

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,17 +1,17 @@
 export class HolbertonClass {
-    constructor(year, location) {
-      this._year = year;
-      this._location = location;
-    }
-  
-    get year() {
-      return this._year;
-    }
-  
-    get location() {
-      return this._location;
-    }
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
   }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
