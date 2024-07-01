d1fecafeb9785de3cac3a27ebafb5a6e77627ae3
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-01 15:36:06 +0100

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,9 @@
-export default getListStudents() {
-
+export default function getListStudents() {
+  let studList = []
+  studList = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
 }
+
