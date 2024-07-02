1d6501dd11fde623a531b007187c4dd7bda22498
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-02 19:35:32 +0100

    task 5 f

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,9 +2,6 @@ export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  const buffer = new ArrayBuffer(length);
-  const int8View = new Uint8Array(buffer, 0, length);
-  int8View[position] = value;
   const dt = new DataView(new ArrayBuffer(length), 0, length);
   dt.setUint8(position, value);
   return dt;
