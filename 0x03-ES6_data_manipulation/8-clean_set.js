5bb57e5e35df1f768f51322a527c1ca63aa842f9
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-03 00:52:04 +0100

    task 8 fixing

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
   const str = [];
