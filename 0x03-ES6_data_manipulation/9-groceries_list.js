e1571d52c39e8271e6871a5389fe25a97a384d1f
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-03 01:25:07 +0100

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  const np = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  //   np.set('Apples', 10);
+  //   np.set('Tomatoes', 10);
+  return np;
+}
