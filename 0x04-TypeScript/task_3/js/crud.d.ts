9791c6e6702c93a2bad2a930e6b3a74b7ace76d4
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-04 01:35:18 +0100

    task 8

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,4 @@
+import { RowID, RowElement } from "./interface";
+export function insertRow(row: RowElement): number; 
+export function deleteRow(rowID: RowID): void; 
+export function updateRow(rowID: RowID, row: RowElement): number; 
