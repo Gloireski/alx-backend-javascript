1fba45ec1750e9863d5bf1c7231f745466bd932e
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-19 16:59:23 +0100

    task 0 fixed

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,10 @@
 /**
  * Displays a message on the STDOUT.
- * @param {String} par The message to display.
- * @author Belem Gloire BEKOUTOU <https://github.com/Gloireski>
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
-const displayMessage = (par) => {
-    console.log(par);
-}
-
-module.exports = displayMessage;
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
