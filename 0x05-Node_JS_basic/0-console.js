3246174e14ef70cb88158cccb0bf5b4e094b4e47
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-19 16:54:42 +0100

    task 0 fixed

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,7 +3,7 @@
  * @param {String} par The message to display.
  * @author Belem Gloire BEKOUTOU <https://github.com/Gloireski>
  */
-function displayMessage (par) {
+const displayMessage = (par) => {
     console.log(par);
 }
 
