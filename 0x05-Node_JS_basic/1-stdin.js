a4d444c7650ddc767876f3ecdfaf95646aa4fd0d
Gloireski <35967799+Gloireski@users.noreply.github.com>
2024-08-22 00:22:09 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,7 +7,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  var entree = process.stdin.read();
+  const entree = process.stdin.read();
 
   if (entree) {
     process.stdout.write(`Your name is: ${entree}`);
