8d92f8a4118a4ad9accdf19d04494c466556827e
Gloireski <35967799+Gloireski@users.noreply.github.com>
2024-08-22 00:28:09 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,13 +7,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+  const entree = process.stdin.read();
 
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  if (entree) {
+    process.stdout.write(`Your name is: ${entree}`);
   }
 });
 
-process.stdin.on('end', function() {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
 });
