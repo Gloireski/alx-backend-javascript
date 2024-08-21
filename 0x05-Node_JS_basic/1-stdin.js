99453ece07a27e50a3a93046d864b11ad42b365c
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-22 00:17:16 +0100

    task 1 fixing2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,7 +10,7 @@ process.stdin.on('readable', () => {
   var entree = process.stdin.read();
 
   if (entree) {
-    process.stdout.write('Your name is: '+ entree);
+    process.stdout.write(`Your name is: ${entree}`);
   }
 });
 
