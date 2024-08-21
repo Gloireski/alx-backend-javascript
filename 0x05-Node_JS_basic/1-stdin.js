1b0e6ceecfc956e18e71ae7c047e047a61774dcd
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-22 00:15:33 +0100

    task 1 fixing1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,14 +6,14 @@
  */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   var entree = process.stdin.read();
 
   if (entree) {
     process.stdout.write('Your name is: '+ entree);
-    }
+  }
 });
 
 process.stdin.on('end', function() {
-    process.stdout.write('This important software is now closing \n');
+    process.stdout.write('This important software is now closing\n');
 });
