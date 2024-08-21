741c9c992687f78b99d2960def77ac5f0d856ed9
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-21 23:50:15 +0100

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+/**
+ * display the message Welcome to Holberton School,
+ * what is your name? (followed by a new line)
+ * program should display Your name is: INPUT
+ * @author Belem Gloire BEKOUTOU <https://github.com/Gloireski>
+ */
+process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name? \n');
+
+process.stdin.on('readable', function() {
+    var entree = process.stdin.read();
+
+    if (entree !== null) {
+        process.stdout.write('Your name is: '+ entree);
+    }
+  });
+  
+process.stdin.on('end', function() {
+    process.stdout.write('This important software is now closing \n');
+  });
